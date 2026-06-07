b31387067eed1af409f34cec4dc592ace103d4b1
Oleksandra Morhun <sashamorgun2002@gmail.com>
2026-06-07 23:53:38 +0300

    changed vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,3 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+export default {
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'harmonizer.lab-x.pp.ua'
+    ]
+  }
+}
